8da822dcb850a74769fc72a3a4e750a6a7b40fb5
AndreiKachur <71757901+AndreiKachur@users.noreply.github.com>
2021-07-10 11:13:48 +0300

    db, hoc, service, reducer, actions were created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import ErrorBoundry from './components/Error-boundry'
+import Restoservice from './services/resto-service'
+import RestoServiceContext from './components/Resto-service-context';
+import reducer from './reducers'
+
+const store = createStore(reducer)
+const restoservice = new Restoservice()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+        <ErrorBoundry>
+            <RestoServiceContext.Provider value={restoservice}>
+                <Router >
+                    <App />
+                </Router>
+            </RestoServiceContext.Provider>
+        </ErrorBoundry>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
