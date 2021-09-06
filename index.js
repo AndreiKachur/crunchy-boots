6859ff0d0f6c1a56eeaca127a9f026b22fbdaaca
Andrei <underfil@mail.ru>
2021-09-06 18:40:17 +0300

    fix git security problem

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const path = require('path')
+// const path = require('path')
 const mongoose = require('mongoose')
 const cors = require('cors');
 const Boots = require('./models/boots.js')
@@ -8,8 +8,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'src')))
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'src')))
 app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3001
@@ -21,7 +21,7 @@ app.get('/boots', async (req, res) => {
 
 async function start() {
     try {
-        const url = 'mongodb+srv://Andrei_K:eCAwwJ74Dhc722CH@cluster0.m7wkh.mongodb.net/Crunchy_Boots'
+        const url = 'mongodb+srv://Andrei_K:PASS@cluster0.m7wkh.mongodb.net/Crunchy_Boots'
         await mongoose.connect(url, {
             useNewUrlParser: true
         })
