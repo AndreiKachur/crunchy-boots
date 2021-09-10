17bb885a276d9482e1bf286ee36251ae664261a7
Andrei <underfil@mail.ru>
2021-09-10 10:57:01 +0300

    tweak responsive markup for form; navbar

--- client/src/components/Form-Sign-In/FormSignup.tsx
+++ client/src/components/Form-Sign-In/FormSignup.tsx
@@ -57,42 +57,6 @@ const FormSignup = ({ submitForm }: any) => {
           />
           {errors.email && <p>{errors.email}</p>}
         </div>
-        <div className='form-inputs'>
-          <label className='form-label'>Password</label>
-          <input
-            className='form-input'
-            type='password'
-            name='password'
-            placeholder='Enter your password'
-            value={values.password}
-            onChange={handleChange}
-          />
-          {errors.password && <p>{errors.password}</p>}
-        </div>
-        <div className='form-inputs'>
-          <label className='form-label'>Confirm Password</label>
-          <input
-            className='form-input'
-            type='password'
-            name='password2'
-            placeholder='Confirm your password'
-            value={values.password2}
-            onChange={handleChange}
-          />
-          {errors.password2 && <p>{errors.password2}</p>}
-        </div>
-        {/* <div className='form-inputs'>
-          <label className='form-label'>Comment for order</label>
-          <input
-            className='form-input'
-            type='text'
-            name='comment'
-            placeholder='Enter your comment'
-            maxLength={60}
-            value={values.comment}
-            onChange={handleChange}
-          />
-        </div> */}
         <button className='form-input-btn' type='submit'>
           Place order
         </button>
