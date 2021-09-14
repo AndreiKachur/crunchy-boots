dfc41ad63e024cde3bfc4fccec45eae249c6e8bf
Andrei <underfil@mail.ru>
2021-09-14 14:24:19 +0300

    fix path

--- server/index.js
+++ server/index.js
@@ -19,12 +19,11 @@ app.use('/login', login)
 app.use('/order', order)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'))
+    app.use(express.static(path.join(__dirname, '../client/build')))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
     })
 }
-
 const PORT = process.env.PORT || keys.BASE_PORT
 
 app.get('/boots', async (req, res) => {
