ed42c782e57fce468303f3ad8b1d8fe91347bb80
Andrei <underfil@mail.ru>
2021-09-14 19:18:26 +0300

    debug deployment

--- client/src/redux/actions/actions-fetch.tsx
+++ client/src/redux/actions/actions-fetch.tsx
@@ -14,11 +14,8 @@ export function fetchBoots(url: string) {
                     throw new Error(`Could not fetch boots,
                                 status ${res.status}`)
                 }
-                return res.json()
+                return res.text()
             })
-            .then((boots: DbItem[]) => dispatch({
-                type: TL.BOOTS_LOADED,
-                payload: boots.sort((a, b) => a.id - b.id)
-            }))
+            .then(r => console.log(r))
     }
 }
