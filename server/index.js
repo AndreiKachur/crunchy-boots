b0a3f4d3d5b6f79c2100df67e63ae8a23b713c45
Andrei <underfil@mail.ru>
2021-09-14 22:19:54 +0300

    add const PORT

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ const keys = require('./keys')
 
 const app = express()
 
+const PORT = process.env.PORT || 5000
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -41,7 +43,7 @@ async function start() {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
-        app.listen(process.env.PORT || 5000, () => {
+        app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         })
 
