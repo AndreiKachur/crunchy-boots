07f3bfb4fe1eefb32559fb501b4f0c342bc792cb
Andrei <underfil@mail.ru>
2021-09-14 11:48:14 +0300

    add production build

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const config = require('config')
+const path = require('path')
 const mongoose = require('mongoose')
 const Boots = require('./models/boots.js')
 const User = require('./models/user.js')
@@ -17,6 +17,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/profile', profile)
 app.use('/login', login)
 app.use('/order', order)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || keys.BASE_PORT
 
