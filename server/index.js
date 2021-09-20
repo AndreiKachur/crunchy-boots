1f154725c5fc155765f2ec0e34529287198eccda
Andrei <underfil@mail.ru>
2021-09-20 11:34:40 +0300

    add sendfile index.html to server side

--- server/index.js
+++ server/index.js
@@ -35,6 +35,10 @@ app.get('/orders', async (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')))
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    })
 }
 
 async function start() {
